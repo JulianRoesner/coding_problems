a24cc54ae4b873311ff1ad7178dc0842758ee92c
JulianRoesner <julian_roesner@gmx.de>
2020-07-20 16:24:38 +0200

    34 Reversing strings with recursion

---
+++ solutions/34_string_reversing_recursion.swift
@@ -0,0 +1,29 @@
+//Reverse a string using recursion
+func reverse(of: String) -> String{
+	if (of.count <= 0){
+		return of
+	}
+	return reverseRec(of: of, iteration: 0)
+}
+
+func reverseRec(of: String, iteration: Int)-> String{
+	if(iteration >= of.count - 1){
+		return String(of[of.index(of.endIndex, offsetBy: -1)])
+	}
+
+	return reverseRec(of: of, iteration: iteration + 1) + String(of[of.index(of.startIndex, offsetBy:(iteration))])
+}
+
+//TESTS
+var empty = ""
+var palindrom = "hannah"
+var teststring = "abcdefg"
+
+var result = reverse(of: empty)
+assert(result == "", "Empty string was not correctly reversed \(result)")
+
+result = reverse(of: palindrom)
+assert(result == "hannah", "palindrom string was not correctly reversed \(result)")
+
+result = reverse(of: teststring)
+assert(result == "gfedcba", "teststring string was not correctly reversed \(result)")
