197c5b5bc487f1d5efddaec43db3d3ee547a3e8b
JulianRoesner <julian_roesner@gmx.de>
2020-07-27 00:52:31 +0200

    58 Intersections between linked lists

--- solutions/58_intersection_linked_lists.swift
+++ solutions/58_intersection_linked_lists.swift
@@ -56,52 +56,84 @@ func findIntersection(between: LinkedList, and: LinkedList) -> Node?{
 		return nil
 	}
 
-	let (_, foundNode) = findIntersection(between: thisRoot, 0 , and: otherRoot, 0)
-	return foundNode
+	let lengthDiff = findLengthDifference(between: thisRoot, 0 , and: otherRoot, 0)
+	
+	return	findIntersection(between: thisRoot, and: otherRoot, lengthDiff: lengthDiff)
 }
 
-func findIntersection(between: Node, _ betweenCount: Int, and: Node, _ andCount: Int) -> (Int, Node?){
-	
+func findLengthDifference(between: Node, _ betweenCount: Int, and: Node, _ andCount: Int) -> Int{
 
-	guard between.next != nil || and.next != nil else{
-		print("\(between.value) / \(and.value) \(and === between)")
-		let diff = betweenCount - andCount
-		if and === between {
-			return (diff, and)
-		}else{
-			return (diff, nil)
-		}
-	}
-	print("\(between.value) / \(and.value)")
-		print("Hallo")
+	guard !(between.next == nil && and.next == nil) else{
+		return betweenCount - andCount
+	} 
 
+	var betweenOffset = 0
 	var nextBetween = between
-	var betweenChange = 0
 	if between.next != nil{
 		nextBetween = between.next!
-		betweenChange += 1
+		betweenOffset += 1
 	}
-	print("Hallo")
 
+	var andOffset = 0
 	var nextAnd = and
-	var andChange = 0
 	if and.next != nil{
 		nextAnd = and.next!
-		andChange += 1
+		andOffset += 1
 	}
 
-	
-	let (lengthDifference, foundNode) = findIntersection(between: nextBetween, betweenCount + betweenChange, and: nextAnd, andCount + andChange)
-	print("\(between.value) / \(and.value) \(and === between)")
-	if and === between {
-		return (lengthDifference, and)
-	}else if foundNode != nil{
-		return (lengthDifference, foundNode)
+	return findLengthDifference(between: nextBetween, betweenCount + betweenOffset, and: nextAnd, andCount + andOffset)
+}
+
+func findIntersection(between: Node, and: Node, lengthDiff: Int ) -> Node?{
+	guard !(between.next == nil && and.next == nil) else{
+		if (lengthDiff == 0 && between === and){
+			return and
+		}else{
+			return nil
+		}
+	}
+	if(lengthDiff > 0){
+		if let nextBetween = between.next{
+			return findIntersection(between: nextBetween, and: and, lengthDiff: lengthDiff - 1)	
+		}else{
+			return findIntersection(between: between, and: and, lengthDiff: lengthDiff - 1)	
+		}
+	}else if(lengthDiff < 0){
+		if let nextAnd = and.next{
+			return findIntersection(between: between, and: nextAnd, lengthDiff: lengthDiff + 1)	
+		}else{
+			return findIntersection(between: between, and: and, lengthDiff: lengthDiff + 1)
+		}
 	}else{
-		return (lengthDifference, nil)
+		if areSame(this: between, andThat: and){
+			return between
+		}else{
+			if between.next != nil && and.next != nil{
+				return findIntersection(between: between.next!, and: and.next!, lengthDiff: lengthDiff )	
+			}else{				
+				return nil
+			}			
+		}
 	}
 }
 
+func areSame(this: Node, andThat: Node) -> Bool{
+	guard !(this.next == nil && andThat.next == nil) else{
+		return this === andThat
+	}
+	var nextThis = this
+	var nextThat = andThat
+
+	if this.next != nil{
+		nextThis = this.next!
+	}
+
+	if andThat.next != nil{
+		nextThat = andThat.next!
+	}
+
+	return this === andThat && areSame(this: nextThis, andThat: nextThat)
+}
 
 //Creating intersection
 var oneElementIntersection = Node(value: 102, next: nil)
@@ -111,7 +143,7 @@ var threeElementIntersection = Node(value: 102, next: Node(value: 103, next: Nod
 var onlyIntersection = LinkedList(root: oneElementIntersection)
 var nodeBeforeIntersection = LinkedList(root: Node(value: 1, next: oneElementIntersection))
 var anotherNodeBeforeIntersection = LinkedList(root: Node(value: 2, next: oneElementIntersection))
-var threeNodesBeforeIntersection = LinkedList(root: Node(value: 1, next: oneElementIntersection))
+var threeNodesBeforeIntersection = LinkedList(root: Node(value: 1, next: Node(value: 17, next: Node(value: 12, next: oneElementIntersection))))
 var emptyList = LinkedList(root: nil)
 var oneElement = LinkedList(root: Node(value: 1, next: nil))
 var manyElements = LinkedList(root: Node(value: 2, next: Node(value: 3, next: Node( value: 4, next: nil))))
@@ -123,7 +155,6 @@ assert(result === oneElementIntersection, "Did find incorrect intersection betwe
 
 result = findIntersection(between: onlyIntersection, and: threeNodesBeforeIntersection)
 assert(result === oneElementIntersection, "Did find incorrect intersection between onlyIntersection and threeNodesBeforeIntersection: \(result as Optional)")
-
 result = findIntersection(between: nodeBeforeIntersection, and: threeNodesBeforeIntersection)
 assert(result === oneElementIntersection, "Did find incorrect intersection between nodeBeforeIntersection and threeNodesBeforeIntersection: \(result as Optional)")
 
@@ -140,13 +171,13 @@ nodeBeforeIntersection = LinkedList(root: Node(value: 1, next: threeElementInter
 threeNodesBeforeIntersection = LinkedList(root: Node(value: 1, next: threeElementIntersection))
 
 result = findIntersection(between: onlyIntersection, and: nodeBeforeIntersection)
-assert(result === threeElementIntersection, "Did find incorrect intersection between onlyIntersection and nodeBeforeIntersection: \(result as Optional)")
+assert(result === threeElementIntersection, "For longer intersection. Did find incorrect intersection between onlyIntersection and nodeBeforeIntersection: \(result as Optional)")
 
 result = findIntersection(between: onlyIntersection, and: threeNodesBeforeIntersection)
-assert(result === threeElementIntersection, "Did find incorrect intersection between onlyIntersection and threeNodesBeforeIntersection: \(result as Optional)")
+assert(result === threeElementIntersection, "For longer intersection. Did find incorrect intersection between onlyIntersection and threeNodesBeforeIntersection: \(result as Optional)")
 
 result = findIntersection(between: nodeBeforeIntersection, and: threeNodesBeforeIntersection)
-assert(result === threeElementIntersection, "Did find incorrect intersection between nodeBeforeIntersection and threeNodesBeforeIntersection: \(result as Optional)")
+assert(result === threeElementIntersection, "For longer intersection. Did find incorrect intersection between nodeBeforeIntersection and threeNodesBeforeIntersection: \(result as Optional)")
 
 
 
