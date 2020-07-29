84a4fa6578a072261ac4e6b7b81d1e13893c1bd6
JulianRoesner <julian_roesner@gmx.de>
2020-07-29 10:38:17 +0200

    78 findin all prime factors

---
+++ solutions/78_find_prime_factors.swift
@@ -0,0 +1,75 @@
+//Sieve of Eratosthenes
+func eratosthenesSieve(of: Int)-> [Int]{
+	guard of >= 2 else{
+		return []
+	}
+	var potentialPrimes : [Int] = Array()
+	for i in 2...of{
+		potentialPrimes.append(i)
+	}
+	var primes : [Int] = Array()
+	for potentialPrime in potentialPrimes{
+		if primes.count > 0{
+			var isPrime = true
+			for prime in primes{
+				if potentialPrime % prime == 0{
+					isPrime = false
+				}
+			}
+			if isPrime{
+				primes.append(potentialPrime)
+			}
+		}else{
+			primes.append(potentialPrime)
+		}
+	}
+
+	return primes
+}
+
+func findPrimeFactors(of: Int)->[Int]{
+	let potentialPrimeFactors = eratosthenesSieve(of: of)
+	var rest = of
+	var primeFactors : [Int] = Array() 
+	for potentialPrime in potentialPrimeFactors{
+		if rest % potentialPrime == 0{
+			primeFactors.append(potentialPrime)
+			rest = Int(rest/potentialPrime)
+		}
+
+		if rest == 1{
+			break
+		}
+	}
+
+	return primeFactors
+}
+
+
+//Test eratosthenesSieve
+var primesSmaller2 : [Int] = []
+var primesUpTo2 = [2]
+var primesUpTo30 = [2,3,5,7,11,13,17,19,23,29]
+
+var result = eratosthenesSieve(of: 1)
+assert(result == primesSmaller2, "Found incorrect primes for 1: \(result) instead of \(primesSmaller2)")
+
+result = eratosthenesSieve(of: 2)
+assert(result == primesUpTo2, "Found incorrect primes for 2: \(result) instead of \(primesUpTo2)")
+
+result = eratosthenesSieve(of: 30)
+assert(result == primesUpTo30, "Found incorrect primes for 30: \(result) instead of \(primesUpTo30)")
+
+//Test finding prime factors
+var factorsOf1: [Int] = []
+var factorsOfAPrime = [13]
+var factorsOfNotAPrime = [2,3,5]
+
+result = findPrimeFactors(of: 1)
+assert(result == factorsOf1, "Found incorrect primes for 1: \(result) instead of \(primesSmaller2)")
+
+result = findPrimeFactors(of: 13)
+assert(result == factorsOfAPrime, "Found incorrect primes for 13: \(result) instead of \(primesSmaller2)")
+
+result = findPrimeFactors(of: 30)
+assert(result == factorsOfNotAPrime, "Found incorrect primes for 40: \(result) instead of \(primesSmaller2)")
